06ef3483ed0b4f0733a1b68ab645f5efa6b28e1a
Mohit Kumhar <mohitmolela@gmail.com>
2023-11-27 19:28:25 +0530

    added search option for students

--- app.py
+++ app.py
@@ -33,6 +33,48 @@ def login():
         messagebox.showerror('Sorry..!!', 'Invalid Login Credential')
 
 
+def search_book_by_author_name():
+    author = author1.get()
+
+    if author:
+        result = collection.find_one({'author': author})
+
+        if result:
+            book_name = result.get('book_name', 'Sorry, Book Information is Not Found')
+            book_author = result.get('author', 'Sorry, Author Information is Not Available')
+            publish_year = result.get('publication_year', 'Sorry, Publish Year is Not Found')
+
+            messagebox.showinfo('FOUND', f"Book Found\nBook Name → '{book_name}'\nWritten By → '{book_author}'\nPublish Year → {publish_year}")
+
+        else:
+            messagebox.showerror('Sorry..!!', f'No Book Found of Author: {author}')
+    else:
+        messagebox.showerror('Sorry..!!', f"Please Provide Author Details")
+
+def search_book_by_publish_year():
+    publish_year = publish_year1.get()
+
+    if publish_year:
+        result = collection.find_one({'publication_year': publish_year})
+        
+        if result:
+            book = result.get('book_name', 'Sorry..!! Book is Not Found')
+            author = result.get('author', 'Sorry..!! Author is Not Found')
+            publication_year = result.get('publication_year', 'Sorry..!! Publish Year is Not Found')
+
+            messagebox.showinfo('SUCCESS', f'Book is Found:\nBook Name → {book}\nAuthor Name → {author}\nPublication Year → {publication_year}')
+
+
+        else:
+            messagebox.showerror('Sorry..!!', 'Not Book Found of This Author')
+
+    else:
+        messagebox.showerror('Sorry..!!', 'Please Provide Publish Year..!!')
+    
+
+
+
+
 def show_admin_dashboard():
     
     def post_book():
@@ -63,8 +105,8 @@ def show_admin_dashboard():
 
     root.withdraw()
     admin_dashboard = tk.Toplevel(root)
-    admin_dashboard.geometry('500x500')
-    admin_dashboard.minsize(400, 450)
+    admin_dashboard.geometry('850x400')
+    admin_dashboard.minsize(400, 650)
     admin_dashboard.config(bg='grey')
     admin_dashboard.title("Admin Dashboard")
     admin_dashboard.iconbitmap('library.ico')
@@ -136,8 +178,8 @@ def show_admin_dashboard():
 
 
 root=Tk()
-root.geometry('500x500')
-root.minsize(400, 450)
+root.geometry('850x400')
+root.minsize(400, 650)
 root.title('Library Management System')
 root.config(background='grey')
 root.iconbitmap('library.ico')
@@ -149,6 +191,10 @@ header.pack(side=TOP, fill=X)
 
 blank_space = Label(root, text="", bg='grey')
 blank_space.pack()
+blank_space = Label(root, text="", bg='grey')
+blank_space.pack()
+blank_space = Label(root, text="", bg='grey')
+blank_space.pack()
 
 
 # Search The Books By Book Number 
@@ -156,7 +202,7 @@ blank_space.pack()
 search_book_student = Frame(root, bg='grey')
 search_book_student.pack()
 
-title1 = Label(search_book_student, text="Search for a Book Name: ", bg='grey', font=('arial',17))
+title1 = Label(search_book_student, text="Search for a Book Name → ", bg='grey', font=('arial',17))
 title1.grid(row=0, column=0)
 
 search1 = Entry(search_book_student, width=30, font=20)
@@ -164,68 +210,43 @@ search1.grid(row=0, column=1)
 
 
 search_button = Button(search_book_student, command=search_book, text='Search', height=1, width=10, fg='black', bg='white', activebackground='black', activeforeground='white', highlightcolor='red', relief=RIDGE, underline=True, background='#A9A9A9', font=('arial', 10, 'bold'))
-# search_button.pack(anchor=NW, padx=120, pady=30)
-search_button.grid(row=0, column=3)
+search_button.grid(row=0, column=3, padx=30)
 
 
-
-blank_space = Label(root, text="", bg='grey')
-blank_space.pack()
+blank_space = Label(search_book_student, text="", bg='grey')
+blank_space.grid(row=1, column=0)
 
 
 # Search Books By Author Name
 
-search_book_student = Frame(root, bg='grey')
-search_book_student.pack()
+title1 = Label(search_book_student, text="Search Books By Author Name → ", bg='grey', font=('arial',17))
+title1.grid(row=3, column=0)
 
-title1 = Label(search_book_student, text="Search Books By Author Name: ", bg='grey', font=('arial',17))
-# title1.pack(anchor=NW, padx=20)
-title1.grid(row=0, column=0)
-
-search1 = Entry(search_book_student, width=30, font=20)
-search1.grid(row=0, column=1)
+author1 = Entry(search_book_student, width=30, font=20)
+author1.grid(row=3, column=1)
 
 
-search_button = Button(search_book_student, command=search_book, text='Search', height=1, width=10, fg='black', bg='white', activebackground='black', activeforeground='white', highlightcolor='red', relief=RIDGE, underline=True, background='#A9A9A9', font=('arial', 10, 'bold'))
-# search_button.pack(anchor=NW, padx=120, pady=30)
-search_button.grid(row=0, column=3)
+search_button = Button(search_book_student, command=search_book_by_author_name, text='Search', height=1, width=10, fg='black', bg='white', activebackground='black', activeforeground='white', highlightcolor='red', relief=RIDGE, underline=True, background='#A9A9A9', font=('arial', 10, 'bold'))
+search_button.grid(row=3, column=3)
 
 
-
-blank_space = Label(root, text="", bg='grey')
-blank_space.pack()
+blank_space = Label(search_book_student, text="", bg='grey')
+blank_space.grid(row=4, column=0)
 
 
 # Search Book By Publish Year
 
-search_book_student = Frame(root, bg='grey')
-search_book_student.pack()
+title1 = Label(search_book_student, text="Search Book By Publish Year → ", bg='grey', font=('arial',17))
+title1.grid(row=5, column=0)
 
-title1 = Label(search_book_student, text="Search Book By Publish Year: ", bg='grey', font=('arial',17))
-# title1.pack(anchor=NW, padx=20)
-title1.grid(row=0, column=0)
-
-search1 = Entry(search_book_student, width=30, font=20)
-search1.grid(row=0, column=1)
+publish_year1 = Entry(search_book_student, width=30, font=20)
+publish_year1.grid(row=5, column=1)
 
 
-search_button = Button(search_book_student, command=search_book, text='Search', height=1, width=10, fg='black', bg='white', activebackground='black', activeforeground='white', highlightcolor='red', relief=RIDGE, underline=True, background='#A9A9A9', font=('arial', 10, 'bold'))
-# search_button.pack(anchor=NW, padx=120, pady=30)
-search_button.grid(row=0, column=3)
-
-
-blank_space = Label(root, text="", bg='grey')
-blank_space.pack()
-
-
-
-
+search_button = Button(search_book_student, command=search_book_by_publish_year, text='Search', height=1, width=10, fg='black', bg='white', activebackground='black', activeforeground='white', highlightcolor='red', relief=RIDGE, underline=True, background='#A9A9A9', font=('arial', 10, 'bold'))
+search_button.grid(row=5, column=3)
 admin_login_button = Button(root, command=login, text='Login', height=1, width=10, fg='black', bg='white', activebackground='black', activeforeground='white', highlightcolor='red', relief=RIDGE, underline=True, background='#A9A9A9', font=('arial', 10, 'bold'))
 admin_login_button.pack(anchor=SW, side=BOTTOM, padx=200)
-
-
-
-
 
 
 login_frame = Frame(root, bg='grey')
@@ -235,35 +256,26 @@ login_frame.pack(anchor=SW, side=BOTTOM)
 
 admin_login = Label(root, text="Admin can Login from Here: ",bg='grey', font=(40))
 admin_login.pack(anchor=SW, side=BOTTOM)
-# admin_login.grid(row=0, column=0)
-
 
 
 enter_username = Label(login_frame, text="Enter Username: ",bg='grey', font=(40))
-# enter_username.pack(anchor=SW, side=BOTTOM)
 enter_username.grid(row=1, column=0, padx=20)
 
+
 admin_username = Entry(login_frame, textvariable='Enter Username: ')
-# username.pack(anchor=SW, side=BOTTOM, padx=200)
 admin_username.grid(row=1, column=1, padx=20)
 
 
 enter_password = Label(login_frame, text="Enter PassWord: ", bg='grey', font=(40))
-# enter_password.pack(anchor=SW, side=BOTTOM)
 enter_password.grid(row=2, column=0)
 
 
-
 admin_password = Entry(login_frame, textvariable='Enter Password: ', show="*")
-# password.pack(anchor=SW, side=BOTTOM, padx=200)
 admin_password.grid(row=2, column=1)
 
 
 blank_space = Label(login_frame, text="", bg='grey')
 blank_space.grid(row=3, column=0)
-
-
-
 
 
 root.mainloop()
